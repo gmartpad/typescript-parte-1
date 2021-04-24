d10a3fbc05e83dd17de5f0c4a59682015b67b87c
gmartpad <gabrielmpwork@gmail.com>
2021-04-24 02:36:27 -0300

    feat: capturando os dados do formulario

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,29 @@
+class NegociacaoController {
+
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor(){
+
+        this._inputData = document.querySelector("#data");
+        this._inputQuantidade = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
+
+    }
+
+    adiciona(event){
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            this._inputData,
+            this._inputQuantidade,
+            this._inputValor
+        );
+
+        console.log(negociacao);
+
+    }
+
+}
